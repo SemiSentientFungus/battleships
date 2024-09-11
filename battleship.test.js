563cf54212b4f9fbf5ea3848730acd87761dc6df
Cassandra <semisentientfungus@gmail.com>
2024-09-11 15:55:26 +0200

    generate first test for ship object

--- battleship.test.js
+++ battleship.test.js
@@ -1,2 +1,9 @@
 const ship = require('./ship');
 
+
+describe('Ship object testing', () => {
+    const boat = new ship(10);
+    test('Ship hp is properly set', () => {
+        expect(boat.hp).toBe(10);
+    })
+})
