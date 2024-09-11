5d7d59d7921aeafd61bf9e04fcafc263dc5ba942
Cassandra <semisentientfungus@gmail.com>
2024-09-11 16:25:26 +0200

    add further testing for sunk

--- battleship.test.js
+++ battleship.test.js
@@ -2,12 +2,19 @@ const ship = require('./ship');
 
 
 describe('Ship object testing', () => {
-    const boat = new ship(10);
+    const boat = new ship(2);
     test('Ship hp is properly set', () => {
-        expect(boat.hp).toBe(10);
+        expect(boat.length  ).toBe(2);
     })
     test('Ship hp decreases on hit', () => {
         boat.hit();
-        expect(boat.hp).toBe(9);
+        expect(boat.hits).toBe(1);
+    })
+    test('Ship can lose hp again', () => {
+        boat.hit();
+        expect(boat.hits).toBe(2);
+    })
+    test('Ship sinks at 0 hp', () => {
+        expect(boat.sunk).toBe(true)
     })
 })
